6bd996002023385647b2aa5572473a328e441986
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-04-26 06:49:15 +0530

    Update ergodicity.py

--- ergodicity.py
+++ ergodicity.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 # import seaborn as sns
 import plotly.express as px
+import altair as alt
 
 st.write("""
 # Ergodicity Experiment
@@ -58,15 +59,19 @@ def run_experiment(initial_amount, gain_pct, loss_pct, leverage):
 
     data_load_state.text('Experiment Completed!')
 
+# Ensemble Average using Altair
 
-    st.write("""
-    ## Ensemble Average
-    """)
-    fig = px.line(df_ens, x="index", y="ens_avg")
-    fig.update_layout(
-        xaxis_title="timestep",
-        yaxis_title="ensemble avg. at timestep",)
-    st.plotly_chart(fig, use_container_width=True)
+st.subheader('Ensemble Average')
+chart4=alt.Chart(df_ens).mark_line().encode(
+                                          x='index',
+                                          y='ens_avg'
+                                          ).configure_legend(
+                                                                         titleFontSize=14,
+                                                                                labelFontSize=10
+                                                                                ) .interactive()
+
+st.altair_chart(chart4,use_container_width=True)
+ 
 
     st.write("""
     ## Specific case (Reality)
