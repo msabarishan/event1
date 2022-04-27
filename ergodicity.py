80bd966e90c9a6dc8d6cedadef3bf2ca485461fd
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-04-27 07:39:57 +0530

    Update ergodicity.py

--- ergodicity.py
+++ ergodicity.py
@@ -60,8 +60,8 @@ data_load_state.text('Experiment Completed!')
 st.write("""
     ## Ensemble Average
     """)
-     fig = px.line(df_ens, x="index", y="ens_avg")
-    fig.update_layout(
+fig = px.line(df_ens, x="index", y="ens_avg")
+fig.update_layout(
         xaxis_title="timestep",
         yaxis_title="ensemble avg. at timestep",)
 st.plotly_chart(fig, use_container_width=True)
@@ -69,19 +69,19 @@ st.plotly_chart(fig, use_container_width=True)
 st.write("""
     ## Specific case (Reality)
     """)
-    rand_p = np.random.randint(1, 100000)
-    fig = px.line(df_gain, x="index", y="p_gain_100")
-    fig.update_layout(
+rand_p = np.random.randint(1, 100000)
+fig = px.line(df_gain, x="index", y="p_gain_100")
+fig.update_layout(
         xaxis_title="timestep",
         yaxis_title="gain at timestep",)
 st.plotly_chart(fig, use_container_width=True)
 
-    st.write("""
+st.write("""
     ## Histogram of money people end up with
     """)
-    residue = df_gain.iloc[-1].value_counts().reset_index()
-    fig = px.histogram(residue, x="index", marginal="box")
-    st.plotly_chart(fig, use_container_width=True)
+residue = df_gain.iloc[-1].value_counts().reset_index()
+fig = px.histogram(residue, x="index", marginal="box")
+st.plotly_chart(fig, use_container_width=True)
 
 sl_initial_amount = st.sidebar.slider('Initial Amount', 1000, 1000000, 1000)
 sl_gain_pct = st.sidebar.slider('Gain %', 0.0, 1.0, 0.5)
